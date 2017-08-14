96cf4bf87a41da6c22184e90d8f90c794eae4494
KrekBuk <admin@margoj.pl>
2017-08-14 17:58:40 +0200

    ups

--- src/main/kotlin/pl/margoj/server/implementation/npc/parser/expressions/OneSideLogicalOperatorExpression.kt
+++ src/main/kotlin/pl/margoj/server/implementation/npc/parser/expressions/OneSideLogicalOperatorExpression.kt
@@ -2,8 +2,7 @@ package pl.margoj.server.implementation.npc.parser.expressions
 
 import pl.margoj.server.implementation.npc.parser.parsed.ScriptContext
 
-class
-OneSideLogicalOperatorExpression(val rightSide: Any, val operator: String) : Expression()
+class OneSideLogicalOperatorExpression(val rightSide: Any, val operator: String) : Expression()
 {
     override fun execute(context: ScriptContext): Any
     {
